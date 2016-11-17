72761c9ce7da61dadcb1dd603715f9f18cdf5d65
João Fernandes <joaovitorfernandes2@gmail.com>
2016-11-16 22:24:18 -0200

    Stage4 completed

--- app/src/main/java/com/example/joaofnds/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/joaofnds/funfacts/FunFactsActivity.java
@@ -1,17 +1,20 @@
 package com.example.joaofnds.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
 	// Declare our View variables
 	private TextView mFactTextView;
 	private Button mShowFactButton;
+	private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,32 +24,17 @@ public class FunFactsActivity extends AppCompatActivity {
 		// Assing the Views fom the layout file corresponding to the variables
 		mFactTextView = (TextView) findViewById(R.id.factTextView);
 		mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 
 		View.OnClickListener listener = new View.OnClickListener() {
 
-        private String[] facts = {
-                "Ants stretch when they wake up in the morning.",
-                "Ostriches can run faster than horses.",
-                "Olympic gold medals are actually made mostly of silver.",
-                "You are born with 300 bones; by the time you are an adult you will have 206.",
-                "It takes about 8 minutes for light from the Sun to reach Earth.",
-                "Some bamboo plants can grow almost a meter in just one day.",
-                "The state of Florida is bigger than England.",
-                "Some penguins can leap 2-3 meters out of the water.",
-                "On average, it takes 66 days to form a new habit.",
-                "Mammoths still walked the earth when the Great Pyramid was being built."
-        };
 			@Override
 			public void onClick(View v) {
-				// The button was clicked, so update de fact TextView with a new fact
-				String fact = "";
-                // Randomly select a fact
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                fact = facts[randomNumber];
-
+                int color = mColorWheel.getColor();
                 // Update the screen with out dynamic fact
-				mFactTextView.setText(fact);
+				mFactTextView.setText(mFactBook.getFact());
+                mRelativeLayout.setBackgroundColor(color);
+                mShowFactButton.setTextColor(color);
 			}
 		};
         mShowFactButton.setOnClickListener(listener);
